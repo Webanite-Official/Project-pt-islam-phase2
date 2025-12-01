41fda9b82a9f1e4e61526c6a013e1ae6d9cb4ef3
Webanite-Official <islambeledy.2007@gmail.com>
2025-12-01 16:11:46 +0200

    Islam 3

---
+++ Actions/AddSWITCH.h
@@ -0,0 +1,27 @@
+#ifndef _ADD_SWITCH_H
+#define _ADD_SWITCH_H
+#include "Action.h"
+#include "..\Components\SWITCH.h"
+class AddSWITCH : public Action
+{
+	private:
+		//Parameters for rectangular area to be occupied by the gate
+		int Cx, Cy;	//Center point of the gate
+		int x1, y1, x2, y2;	//Two corners of the rectangluar area
+public:	
+	AddSWITCH(ApplicationManager *pApp);
+	virtual ~AddSWITCH(void);
+	
+	//Reads SWITCH parameters
+	virtual void ReadActionParameters();
+	
+	//Execute action (code depends on action type)
+	virtual void Execute();
+	
+	//To undo this action (code depends on action type)
+	virtual void Undo();
+	
+	//To redo this action (code depends on action type)
+	virtual void Redo();
+};
+#endif
