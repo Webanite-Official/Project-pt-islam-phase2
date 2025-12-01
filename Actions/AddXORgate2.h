499aab6e26f0caa1f8a011a24a56ce4ade596efe
biso660 <mahmoudbael6@gmail.com>
2025-12-01 16:09:50 +0200

    basel 1

--- Actions/AddXORgate2.h
+++ Actions/AddXORgate2.h
@@ -9,6 +9,51 @@ class AddXORgate2 : public Action
 private:
 	//Parameters for rectangular area to be occupied by the gate
 	int Cx, Cy;	//Center point of the gate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	int x1, y1, x2, y2;	//Two corners of the rectangluar area
 public:
 	AddXORgate2(ApplicationManager* pApp);
